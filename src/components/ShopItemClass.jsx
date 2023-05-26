82afbcfcad5b9ff47a350a74e223ed1f29c02298
sitt1npretty <orlovnikita7111@yandex.ru>
2023-05-26 11:08:34 +0300

    change

--- src/components/ShopItemClass.jsx
+++ src/components/ShopItemClass.jsx
@@ -1,16 +1,9 @@
 import React, { Component } from 'react'
 
-const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
-  }
-
 export default class ShopItemClass extends Component {
   render() {
+    
+    const {item}=this.props
     return (
         <div className="main-content">
         <h2>{item.brand}</h2>
